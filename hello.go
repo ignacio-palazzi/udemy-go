0f53ecc9e4053743a290bd3bee41d810d05ab5c5
Gabriel Ignacio Palazzi <ignacio.palazzi97@gmail.com>
2024-10-30 16:20:06 -0300

    "Added constant declarations, variable declarations, and print statements to hello.go"

--- hello.go
+++ hello.go
@@ -6,6 +6,32 @@ import (
 	"rsc.io/quote"
 )
 
+// Declaracion de constante
+const Pi float32 = 3.14
+
+const (
+	Domingo = iota + 1
+	Lunes
+	Martes
+	Miercoles
+	Jueves
+	Viernes
+	Sabado
+)
+
 func main() {
+	fmt.Println("Hola Mundo")
 	fmt.Println(quote.Hello())
+
+	//Declaracion de Variables
+	var firstName, lastName string
+	var age uint8
+
+	firstName = "Ignacio"
+	lastName = "Palazzi"
+	age = 27
+
+	fmt.Println(firstName, lastName, "Edad:", age)
+	fmt.Println(Pi)
+	fmt.Println(Viernes)
 }
